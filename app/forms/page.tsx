306c87048a28fb27f3ff5e8b5942654bb966b35d
Ashley Garriga <140339056+Garriga11@users.noreply.github.com>
2025-02-25 17:29:53 -0600

    Add files via upload

---
+++ app/forms/page.tsx
@@ -0,0 +1,11 @@
+// app/dashboard/page.tsx
+import styles from './page.module.css';  // Import the CSS module
+
+export default function forms() {
+    return (
+        <div className={styles.dashboardContainer}>
+            <h1 className={styles.dashboardTitle}>Dashboard</h1>
+            <p className={styles.dashboardText}>Welcome to the dashboard.</p>
+        </div>
+    );
+}
