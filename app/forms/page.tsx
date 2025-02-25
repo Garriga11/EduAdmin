b419fbaed2aeb5a81c195f70983419b5a74be01c
Ashley Garriga <140339056+Garriga11@users.noreply.github.com>
2025-02-25 17:32:55 -0600

    Update page.tsx

--- app/forms/page.tsx
+++ app/forms/page.tsx
@@ -3,9 +3,9 @@ import styles from './page.module.css';  // Import the CSS module
 
 export default function forms() {
     return (
-        <div className={styles.dashboardContainer}>
-            <h1 className={styles.dashboardTitle}>Dashboard</h1>
-            <p className={styles.dashboardText}>Welcome to the dashboard.</p>
+        <div className={styles.formsContainer}>
+            <h1 className={styles.formsTitle}>Dashboard</h1>
+            <p className={styles.formsText}>Welcome to the dashboard.</p>
         </div>
     );
 }
