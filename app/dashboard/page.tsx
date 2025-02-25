926913ed7a5755488c6f19eab4a6450989dba0b8
Ashley Garriga <140339056+Garriga11@users.noreply.github.com>
2025-02-25 16:48:21 -0600

    Update page.tsx

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,10 +2,10 @@
 import styles from './page.module.css';  // Import the CSS module
 
 export default function Dashboard() {
-    return (
-        <div className={styles.dashboardContainer}>
-            <h1 className={styles.dashboardTitle}>Dashboard</h1>
-            <p className={styles.dashboardText}>Welcome to the dashboard.</p>
-        </div>
-    );
+  return (
+    <div className={styles.dashboardContainer}>
+      <h1 className={styles.dashboardTitle}>Dashboard</h1>
+      <p className={styles.dashboardText}>Welcome to the dashboard.</p>
+    </div>
+  );
 }
