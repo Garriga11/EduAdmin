1ce9d879bb3cfe5371dc70c826aba0af5d977489
Ashley Garriga <140339056+Garriga11@users.noreply.github.com>
2025-02-27 03:14:58 -0600

    Update page.tsx

--- app/forms/page.tsx
+++ app/forms/page.tsx
@@ -5,7 +5,7 @@ export default function forms() {
     return (
         <div className={styles.formsContainer}>
             <h1 className={styles.formsTitle}>Forms</h1>
-            <p className={styles.formsText}>Welcome to the dashboard.</p>
+            <p className={styles.formsText}>Welcome to Forms.</p>
         </div>
     );
 }
