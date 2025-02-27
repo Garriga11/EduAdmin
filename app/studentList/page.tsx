6e748fd0fc40d512dc7a1875a7ecb6674023019e
Ashley Garriga <140339056+Garriga11@users.noreply.github.com>
2025-02-27 03:25:46 -0600

    Update page.tsx

--- app/studentList/page.tsx
+++ app/studentList/page.tsx
@@ -4,7 +4,7 @@ import styles from './page.module.css';  // Import the CSS module
 export default function students() {
     return (
         <div className={styles.studentsContainer}>
-            <h1 className={styles.studentTitle}>Student Lists</h1>
+            <h1 className={styles.studentTitle}>student List </h1>
             <p className={styles.studentsText}>Student Lists.</p>        </div>
     );
 }
